6ccaa3bf558ef7a6dc9fe6cbfac772b15ff78e4b
KeunwooPark <keunwoopark.524@gmail.com>
2020-12-22 10:26:22 +0900

    add train.py

--- siren/data_loader.py
+++ siren/data_loader.py
@@ -2,16 +2,21 @@ from PIL import Image
 import numpy as np
 
 class ImageLoader:
-    def __init__(self, img_path, do_batch=False, batch_size=256):
-        img = Image.open(image_path)
-        self.img = normalize_img(np.array(img))
+    def __init__(self, img_path, size=0, do_batch=False, batch_size=256):
+        img = Image.open(img_path)
         
+        if size > 0:
+            img = img.resize((size, size))
+
+        self.normalized_img = normalize_img(np.array(img))
+         
         self.do_batch = do_batch
         self.batch_size = batch_size
 
-    def get(self, idx):
-        pass
+        self.x, self.y = image_to_xy(self.normalize_img)
 
+    def get(self, i):
+        pass
 
 def normalize_img(img_array):
     return (img_array - 0.5) / 0.5
@@ -20,4 +25,27 @@ def unnormalize_img(img_array):
     return img_array * 0.5 + 0.5
 
 def image_to_xy(img_array):
-    pass
+    width, height, channel = img_array.shape
+    x = []
+    y = []
+
+    for i in range(width):
+        for j in range(height):
+            x.append([i, j])
+            y.append(img_array[i, j])
+    return np.array(x), np.array(y)
+
+def split_to_batches(x, y, size = 0):
+    if size == 0:
+        num_batches = 1
+        return [x], [y], num_batches
+
+    num_sample = x.shape[0]
+    num_batches = num_sample // size
+    if not num_sample % size == 0:
+        num_batches += 1
+
+    batched_x = np.split(x, num_batches)
+    batched_y = np.split(y, num_batches)
+
+    return batched_x, batched_y, num_batches
