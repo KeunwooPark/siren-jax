e49b175297be5c3b22b52b638c0801c117042e71
Keunwoo Park <keunwoopark.524@gmail.com>
2020-12-22 11:05:13 +0900

    add update data_loader.py

--- siren/data_loader.py
+++ siren/data_loader.py
@@ -4,19 +4,39 @@ import numpy as np
 class ImageLoader:
     def __init__(self, img_path, size=0, do_batch=False, batch_size=256):
         img = Image.open(img_path)
-        
+
         if size > 0:
             img = img.resize((size, size))
 
         self.normalized_img = normalize_img(np.array(img))
-         
+
         self.do_batch = do_batch
         self.batch_size = batch_size
 
-        self.x, self.y = image_to_xy(self.normalize_img)
+        x, y = image_to_xy(self.normalized_img)
+        self.batched_x, self.batched_y, self.num_batches = split_to_batches(x, y, size = batch_size)
+        self.cursor = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            data = self.get(self.cursor)
+        except IndexError:
+            raise StopIteration
+
+        self.cursor += 1
+        return data
+
+    def __len__(self):
+        return self.num_batches
 
     def get(self, i):
-        pass
+        x = self.batched_x[i]
+        y = self.batched_y[i]
+        data = {'input': x, 'output': y}
+        return data
 
 def normalize_img(img_array):
     return (img_array - 0.5) / 0.5
@@ -35,7 +55,11 @@ def image_to_xy(img_array):
             y.append(img_array[i, j])
     return np.array(x), np.array(y)
 
-def split_to_batches(x, y, size = 0):
+def split_to_batches(x, y, size = 0, shuffle=True):
+    if shuffle:
+        x = shuffle_array(x)
+        y = shuffle_array(y)
+
     if size == 0:
         num_batches = 1
         return [x], [y], num_batches
@@ -49,3 +73,9 @@ def split_to_batches(x, y, size = 0):
     batched_y = np.split(y, num_batches)
 
     return batched_x, batched_y, num_batches
+
+def shuffle_array(array):
+    size = array.shape[0]
+    indicies = np.arange(size, dtype=np.int)
+    np.random.shuffle(indicies)
+    return array[indicies]
--- train.py
+++ train.py
@@ -3,19 +3,22 @@ from siren.data_loader import ImageLoader
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train SirenHighres')
-    
-    parser.add_argument('--file', type=str, help="location of the file")
+
+    parser.add_argument('--file', type=str, help="location of the file", required=True)
     parser.add_argument('--size', type=int, default=256, help="resize the image to this (squre) shape. 0 if not goint go resize")
     parser.add_argument('--do_batch', action='store_true', default=False, help="separate input to batches")
     parser.add_argument('--batch_size', type=int, default=256, help="the size of batches. only valid when --do_batch")
     parser.add_argument('--epoch', type=int, default=1000, help="number of epochs")
-    
+
     args = parser.parse_args()
     return args
 
 def main(args):
-    image_loader = ImageLoader(args.file, args.downsample, args.do_batch, args.batch_size)
-    print(args)
+    for _ in range(args.epoch):
+        image_loader = ImageLoader(args.file, args.size, args.do_batch, args.batch_size)
+        for i, data in enumerate(image_loader):
+            pass
+
 
 if __name__ == "__main__":
     args = parse_args()
