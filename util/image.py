676b8792fcd2381132d0fe4a8b248a90cb627741
KeunwooPark <keunwoopark.524@gmail.com>
2021-01-02 15:32:13 +0900

    add laplace to image

--- util/image.py
+++ util/image.py
@@ -1,12 +1,17 @@
 from scipy import ndimage
 import numpy as np
 import matplotlib.colors as colors
+import cv2
+import cmapy
 
 def gradient(img):
     grad_x = ndimage.sobel(img, axis=1)
     grad_y = ndimage.sobel(img, axis=0)
     
     return np.concatenate([grad_y, grad_x], axis = -1)
+
+def laplace(img):
+    return ndimage.laplace(img)
 
 def rescale_img(x, max_val = 1.0, min_val = 0.0):
     x_min = np.min(x)
@@ -15,6 +20,13 @@ def rescale_img(x, max_val = 1.0, min_val = 0.0):
     rescaled = (x - x_min) / (x_max - x_min) * (max_val - min_val) + min_val
 
     return rescaled
+
+def clip_img_by_perc(x, perc):
+    x_min = np.percentile(x, perc)
+    x_max = np.percentile(x, 100 - perc)
+    x = np.clip(x, x_min, x_max)
+
+    return x
 
 def gradient_to_img(gradient):
     # code from original Siren implementation
@@ -43,3 +55,11 @@ def gradient_to_img(gradient):
     rgb = colors.hsv_to_rgb(hsv)
     rgb *= 255
     return rgb
+
+def laplace_to_img(laplace):
+    laplace = clip_img_by_perc(laplace, 2)
+    rescaled = rescale_img(laplace, max_val=255, min_val=0)
+    
+    colormap_img = cv2.applyColorMap(np.uint8(rescaled).squeeze(), cmapy.cmap('RdBu'))
+    img = cv2.cvtColor(colormap_img, cv2.COLOR_BGR2RGB)
+    return img
